dcf806d4ccf7c331f80838acd2cc9c66f60089d4
WindHouse <155715318+PingPlayVN@users.noreply.github.com>
2026-02-07 08:06:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker
 
-const CACHE_NAME = 'wind-share-v2.3'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
+const CACHE_NAME = 'wind-share-v2.4'; // Đổi tên này nếu bạn update code để ép trình duyệt tải lại
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -59,4 +59,5 @@ self.addEventListener('fetch', (e) => {
     );
 
 });
+
 
